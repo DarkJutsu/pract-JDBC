f4c3a6317e131cab2fdd48a37222cf1d72d9a3b2
DarkJutsu <ulloajose34@gmail.com>
2024-07-03 21:34:34 -0600

    feat(main):Add Properties & use in getConnection

--- src/main/java/samllo/ejemplo/jdbc/Main.java
+++ src/main/java/samllo/ejemplo/jdbc/Main.java
@@ -1,16 +1,24 @@
 package samllo.ejemplo.jdbc;
 
 import java.sql.*;
+import java.util.Properties;
 
 public class Main {
+    private static Properties dataBaseProperties() {
+        Properties p = new Properties();
+        p.setProperty("user", "sa");
+        p.setProperty("password", "123");
+        return p;
+    }
+
     public static void main(String[] args) throws SQLException {
 /*      MOSTRAR LOS DRIVERS INSTALADOS
         for (Driver d : DriverManager.drivers().toList()) {
             System.out.println(d.toString());
         }
 */
-
-        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/TestJDBC", "sa", "123");
+        Properties props = dataBaseProperties();
+        Connection con = DriverManager.getConnection("jdbc:postgresql://localhost:5432/TestJDBC", props);
         try (Statement st = con.createStatement(); ResultSet rs = st.executeQuery("select * from alumno")) {
             while (rs.next()) {
                 String nombre = rs.getString("nombre");
